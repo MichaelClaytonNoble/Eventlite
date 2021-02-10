ceddf13c8f17492c073f0fc9cd66c573335e6c30
Michael Clayton Noble <makos.code@gmail.com>
2021-02-10 14:07:03 -0800

    change store to conditionally add logger middle ware when not in production mode

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root';
 import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  const middlewares = [thunk];
+
+  if (process.env.NODE_ENV !== "production") {
+    // must use 'require' (import only allowed at top of file)
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+  }
+  return createStore(rootReducer, preloadedState, applyMiddleware(middlewares));
 }
 
 export default configureStore;
