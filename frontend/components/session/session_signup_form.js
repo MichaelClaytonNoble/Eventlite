5b97bb5bc8659207180cc99c96d9bc92f9f104b3
Michael Clayton Noble <makos.code@gmail.com>
2021-02-11 12:19:27 -0800

    create sign up form

--- frontend/components/session/session_signup_form.js
+++ frontend/components/session/session_signup_form.js
@@ -1,1 +1,33 @@
 
+import React from 'react';
+
+class SessionSignupForm extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div id="session-signup-form">
+        <h1 id="header">Welcome back</h1>
+        <form id="session-signup-info-form">
+
+          <input id="session-form-input" type="text" placeholder="Confirm email" />
+
+          <span id="session-signup-info-form-name">
+            <input id="session-form-half-input" type="text" placeholder="First Name" />
+            <input id="session-form-half-input" type="text" placeholder="Last Name" />
+          </span>
+
+          <input id="session-form-input" type="password" placeholder="Password" /> 
+
+          <p id="">Your password must be at least 8 characters</p>
+          button = <button id="session-form-submit">Sign Up</button>
+        </form>
+        <Link to="/signin/login">Log In Instead</Link>
+      </div>
+    )
+  }
+}
+
+export default SessionSignupForm;
