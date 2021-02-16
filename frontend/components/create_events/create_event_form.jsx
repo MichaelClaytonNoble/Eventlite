ea40fd7c6a5ee71e897ab8e2267b48d9ccf110b1
Michael Clayton Noble <makos.code@gmail.com>
2021-02-15 17:39:29 -0800

    creating form for create event

--- frontend/components/create_events/create_event_form.jsx
+++ frontend/components/create_events/create_event_form.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class CreateEventForm extends React.Component{
   constructor(props){
     super(props);
+
   }
 
   render(){
@@ -16,8 +17,11 @@ class CreateEventForm extends React.Component{
 
             <input className="large-input"
                   placeholder="Event Title"/>
-            <input className="large-input"
-                  placeholder ="Organizer"/>
+            
+            <label className="large-input">Organizer
+              <input className="large-input"
+                    placeholder ="Organizer"/>
+            </label>
             
             <select name="categories" id="categories">
               <option value="" >Category</option>
@@ -29,7 +33,7 @@ class CreateEventForm extends React.Component{
         <h1 id="create-event-header">Location</h1>
         <p id="create-event-description">Help people in the area discover your event and let attendees know where to show up.</p>
           
-        <div id="location-radio-buttons">
+        <div id="create-event-radio-buttons">
 
           <input type="radio" id="VENUE" name="location" value="VENUE" />
           <label htmlFor="VENUE">Venue</label>
@@ -44,11 +48,23 @@ class CreateEventForm extends React.Component{
         </section>
         <section className="info-section">
 
-        </section>
         <h1 id="create-event-header">Date and time</h1>
         <p id="create-event-description">Tell event-goers when your event starts and ends so they can make plans to attend.</p>
 
-        
+          <div id="create-event-radio-buttons">
+            <input type="radio" id="single" name="recurring" value="false" />
+            <label htmlFor="single">Single Event</label>
+            <input type="radio" id="recurring" name="recurring" value="true" />
+            <label htmlFor="recurring">Recurring Events</label>
+          </div>
+
+        <label id="event-time">Event starts<br />
+          <input type="datetime-local" id="date-input" />
+        </label>
+        <label id="event-time">Event ends<br />
+          <input type="datetime-local" id="date-input" />
+        </label>
+        </section>
         </form>
       </div>
     )
