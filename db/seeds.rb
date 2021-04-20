cecf0dd975d2649749c3c02ffe79823f29b3b6c5
MichaelClaytonNoble <makos.code@gmail.com>
2021-04-20 14:43:22 -0700

    update seed file add simple animation to event show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
  require 'faker'
 
 
+def seedUser
+  User.destroy_all
 
-User.destroy_all
-
-User.create!({id: 1, email: "Example.Account@demo.com", password: "123456", first_name: "Demo", last_name: "Login"})
-User.create!({id: 2, email: "000@000.000", password: "123456", first_name: "000", last_name: "000"})
-User.create!({id: 3, email: "111@111.111", password: "123456", first_name: "111", last_name: "111"}) 
-User.create!({id: 4, email: "222@222.222", password: "123456", first_name: "222", last_name: "222"}) 
-
+  User.create!({id: 1, email: "Example.Account@demo.com", password: "123456", first_name: "Demo", last_name: "Login"})
+  User.create!({id: 2, email: "000@000.000", password: "123456", first_name: "000", last_name: "000"})
+  User.create!({id: 3, email: "111@111.111", password: "123456", first_name: "111", last_name: "111"}) 
+  User.create!({id: 4, email: "222@222.222", password: "123456", first_name: "222", last_name: "222"}) 
+end
 
 # Event.create!({id: , title: , 
 # description: , 
@@ -50,8 +50,6 @@ def details(n)
   else
     return -1
   end
-
-
 end
 
 def food_and_drink
@@ -115,7 +113,7 @@ def performing_and_visual_arts
   artist = Faker::Artist
   title = "Recreate history with " + artist.name
   about = "Watch a live reconstruction of " + artist.name + "'s greatest work."
-  description = "Come for a holistic day of education and history in your local museum. This event is great for kids" 
+  description = "Come for a holistic day of education and history in your local museum. ]This event is great for kids" 
   return {'title'=> title, 'about'=>about, 'description'=>description}
 end
 
@@ -138,82 +136,110 @@ category_path = {
     8=> "science_technology"
 }
 
-Event.destroy_all
-locations = ['ONLINE', 'VENUE', 'TBA']
+def seedEvents 
+
+  Event.destroy_all
+  locations = ['ONLINE', 'VENUE', 'TBA']
 
 
-idx = 0
-(1..8).each do |category|
-  (0...15).each do |i|
+  idx = 0
+  (1..8).each do |category|
+    (0...15).each do |i|
 
-    location = locations.sample
-    venue = ''
-    if location == 'VENUE'
-      venue = Faker::Address.full_address
+      location = locations.sample
+      venue = ''
+      if location == 'VENUE'
+        venue = Faker::Address.full_address
+      end
+      start = Date.today + rand(1...29)
+      stopDate = start + rand(1...7)
+      creator = rand(1..2)
+      
+      detail = details(category)
+      description_with_lorem = detail['description']
+      description_with_lorem += '\n'
+      description_with_lorem += Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+      description_with_lorem += '\n'
+      description_with_lorem += Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+      description_with_lorem += '\n'
+      description_with_lorem += Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+      
+      e = Event.create!({id: idx, 
+        title: detail['title'], 
+        description: description_with_lorem, 
+        about: detail['about'],
+        category_id: category, 
+        location: location, 
+        venue: venue, 
+        recurring: [false, true].sample, 
+        start: start, 
+        end: stopDate, 
+        timezone: 'PST', 
+        creator_id: creator})
+
+        j = i+1
+        path = 'app/assets/images/' + category_path[category] + '/' + j.to_s + ".jpeg"
+        filename = j.to_s + '.jpeg'
+        e.image.attach(io: File.open(path), filename: filename)
+        idx+=1
     end
-    start = Date.today + rand(1...29)
-    stopDate = start + rand(1...7)
-    creator = rand(1..2)
-    
-    detail = details(category)
-
-    e = Event.create!({id: idx, 
-      title: detail['title'], 
-      description: detail['description'], 
-      about: detail['about'],
-      category_id: category, 
-      location: location, 
-      venue: venue, 
-      recurring: [false, true].sample, 
-      start: start, 
-      end: stopDate, 
-      timezone: 'PST', 
-      creator_id: creator})
-
-      j = i+1
-      path = 'app/assets/images/' + category_path[category] + '/' + j.to_s + ".jpeg"
-      filename = j.to_s + '.jpeg'
-      e.image.attach(io: File.open(path), filename: filename)
-      idx+=1
   end
 end
 
 
-Category.destroy_all
+def seedCategory
 
-Category.create!({id: 1, name: "Food & Drink"})
-Category.create!({id: 2, name: "Health"})
-Category.create!({id: 3, name: "Community"})
-Category.create!({id: 4, name: "Film & Media"})
-Category.create!({id: 5, name: "Travel & Outdoor"})
-Category.create!({id: 6, name: "Music"})
-Category.create!({id: 7, name: "Performing & Visual Arts"})
-Category.create!({id: 8, name: "Science & Tech"})
+  Category.destroy_all
+
+  Category.create!({id: 1, name: "Food & Drink"})
+  Category.create!({id: 2, name: "Health"})
+  Category.create!({id: 3, name: "Community"})
+  Category.create!({id: 4, name: "Film & Media"})
+  Category.create!({id: 5, name: "Travel & Outdoor"})
+  Category.create!({id: 6, name: "Music"})
+  Category.create!({id: 7, name: "Performing & Visual Arts"})
+  Category.create!({id: 8, name: "Science & Tech"})
+end
 
 
+def seedFeaturedCollection
+  FeaturedCollection.destroy_all
 
-FeaturedCollection.destroy_all
+  FeaturedCollection.create!({id: 1,
+  title: "Educate Yourself: Online Racial Equity Workshops",
+  description: "Black History Month is a time for celebrating Black achievement. Black History Month is a time is celebrate and remember important people that are a part of this African diaspora. Come celebrate our brothers by learning and participating in one of many racial equity and history workshops.",
+  collection_name: "Black History Month"})
+  FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/black_history_month.jpeg'), filename: 'black_history_month.jpeg')
 
-FeaturedCollection.create!({id: 1,
-title: "Educate Yourself: Online Racial Equity Workshops",
-description: "Black History Month is a time for celebrating Black achievement. Black History Month is a time is celebrate and remember important people that are a part of this African diaspora. Come celebrate our brothers by learning and participating in one of many racial equity and history workshops.",
-collection_name: "Black History Month"})
-FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/black_history_month.jpeg'), filename: 'black_history_month.jpeg')
+  FeaturedCollection.create!({id: 2,
+  title: "The Music Never Stops: Virtual Concerts &  DJ Sets",
+  description: "Live music lives mostly online these days, but if you're in need of a musical fix, there's still plenty of streaming concerts, virtual dance parties, vocal competitions, history lessons, and music-making classes to keep you dancing in your home.",
+  collection_name: "Live Music"})
+  FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/live_music.jpeg'), filename: 'live_music.jpeg')
 
-FeaturedCollection.create!({id: 2,
-title: "The Music Never Stops: Virtual Concerts &  DJ Sets",
-description: "Live music lives mostly online these days, but if you're in need of a musical fix, there's still plenty of streaming concerts, virtual dance parties, vocal competitions, history lessons, and music-making classes to keep you dancing in your home.",
-collection_name: "Live Music"})
-FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/live_music.jpeg'), filename: 'live_music.jpeg')
+  FeaturedCollection.create!({id: 3,
+  title: "Learn to Cook: Online cooking classes & More",
+  description: "Struggling to entertain yourself while trying to work from home? Eventlite offers a plethora of online classes and fun activities to help keep yourself busy. There's a virtual world of culinary courses and more.",
+  collection_name: "Learn to make"})
+  FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/learn_to_cook.jpeg'), filename: 'learn_to_cook.jpeg')
 
-FeaturedCollection.create!({id: 3,
-title: "Learn to Cook: Online cooking classes & More",
-description: "Struggling to entertain yourself while trying to work from home? Eventlite offers a plethora of online classes and fun activities to help keep yourself busy. There's a virtual world of culinary courses and more.",
-collection_name: "Learn to make"})
-FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/learn_to_cook.jpeg'), filename: 'learn_to_cook.jpeg')
+  FeaturedCollection.create!({id: 4,
+  title: "Tour the world: Online museum exhibits & more",
+  description:"Even the most active sightseer and museum goer needs a break sometime. Enjoy the wonders of the world without leaving your couch. Go to Egypts amazing pyramid or attend the grand opening of your local museum all with a click of your mouse. Let this curated collection serve as your one-stop shop for all things virtual.",
+  collection_name: "Culture"})
+  FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/culture.jpeg'), filename: 'culture.jpeg')
+end
 
-FeaturedCollection.create!({id: 4,
-title: "Tour the world: Online museum exhibits & more",
-description:"Even the most active sightseer and museum goer needs a break sometime. Enjoy the wonders of the world without leaving your couch. Go to Egypts amazing pyramid or attend the grand opening of your local museum all with a click of your mouse. Let this curated collection serve as your one-stop shop for all things virtual.",
-collection_name: "Culture"})
-FeaturedCollection.last.image.attach(io: File.open('app/assets/images/featured_collections/culture.jpeg'), filename: 'culture.jpeg')
+def addDescriptionToEvents
+  Event.all.each do |event|
+    description_with_lorem = event['description']
+    description_with_lorem += '\n'
+    description_with_lorem += Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+    description_with_lorem += '\n'
+    description_with_lorem += Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+    description_with_lorem += '\n'
+    description_with_lorem += Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+    event.update(description: description_with_lorem)
+  end
+end
+
