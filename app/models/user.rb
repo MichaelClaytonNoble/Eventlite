5ed42bcbb9ab6003c309676fdfb2ce2e372eb1e6
MichaelClaytonNoble <makos.code@gmail.com>
2021-05-19 22:15:09 -0700

    add model level email validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6, allow_nil: true}
   validates :email, :first_name, :last_name, :password_digest, :session_token, presence: true
   validates :email, :session_token, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
   attr_reader :password
   after_initialize :ensure_session_token!
