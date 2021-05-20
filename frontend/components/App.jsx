732403e830d5f952acf5e2010bd418fd31444eed
MichaelClaytonNoble <makos.code@gmail.com>
2021-05-19 21:05:27 -0700

    create basic about me page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -24,6 +24,7 @@ import ShowEventContainer from './show_event/show_event_container.js';
 import {useLocation} from 'react-router-dom'; 
 import ShowLikesContainer from './profile/show_likes_container.js';
 import CreateTicketFormContainer from './create_tickets/create_ticket_form_container.js';
+import About from './about/about.jsx';
 
 
 // usePageViews(){
@@ -59,6 +60,7 @@ class App extends React.Component{
           <Route path="/events/browse/" component={BrowseEventsContainer} />
           <Route path="/events/:eventId" component={ShowEventContainer} />
 
+          <Route path="/about" component={About} />
           <Route exact path="/404" component={PageNotFound} />
           <Redirect to="/404" />
 
---
+++ frontend/components/about/about.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const About = () => {
+
+  return (
+    <div id="About">
+      For more information about how this site works please visit my github. 
+      <a className="about-link" target="_blank" href="http://github.com/MichaelClaytonNoble/Eventlite"> here </a>
+      <br />
+      <br />
+      <br />
+      Please check out my linkedin.
+      <a className="about-link" target="_blank" href="http://www.linkedin.com/in/michaelclaytonnoble"> here </a>
+      <br />
+      <br />
+      <br />
+
+      or view my personal site.
+      <a className="about-link" target="_blank" href="http://MichaelClaytonNoble.github.io/"> here </a>
+      <br />
+      <br />
+      <br />
+
+    </div>
+  )
+
+};
+
+export default About;
